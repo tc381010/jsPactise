50e2fdb800678585509fa7bf4ed0f9c74675c5fd
duke chen <tc381010@gmail.com>
2023-03-27 16:48:20 +0800

    練習一 打印1~9

---
+++ practise1.js
@@ -0,0 +1,36 @@
+// 練習一：印出一到九
+
+// 請你分別用 for loop 以及 while 迴圈，
+// 印出 1~9。
+
+// 預期輸出：
+
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+
+for( let i = 1 ; i <= 9 ; i++){
+    console.log(i)
+}
+
+console.log('-------------------')
+
+let j = 11;
+while (j <= 19){
+    console.log(j)
+    j++
+}
+
+console.log('-------------------')
+
+let k = 21;
+do{
+    console.log(k);
+    k++;
+}while(k <= 29)
